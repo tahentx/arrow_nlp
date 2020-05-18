6e4dad323c556ec5c4a383cb7cd46790eec82a9b
Todd Hendricks <hendricks.ta@gmail.com>
2020-05-18 15:28:59 -0700

    init project

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Topic modeling the Apache Arrow repo (commit comments and pull requests) using non-negative matrix factorization.',
+    author='Todd Hendricks',
+    license='',
+)
